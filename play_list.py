c93444cb6281287c1d63860288a9a557f9efbe07
Akavall <akavall@gmail.com>
2015-10-11 12:24:25 -0500

    added mp3 case

--- play_list.py
+++ play_list.py
@@ -16,19 +16,19 @@ def get_wav_duration(file_name):
         return duration
 
 def get_mp3_duration(file_name):
-    return eyed3.load("06 Wonderful, Amazing.mp3").info.time_secs
+    return eyed3.load(file_name).info.time_secs
 
 print "Will play: {}".format(sys.argv[1:])
 
 for file_name in sys.argv[1:]:
-    if file_name.split(".") == "wav":
+    if file_name[-3:] == "wav":
         logging.info("Now playing: {}".format(file_name))
         duration = get_wav_duration(file_name)
-        os.system("gnome-open {}".format(file_name))
+        os.system("gnome-open \"{}\"".format(file_name))
         time.sleep(duration + 1)
-    elif file_name.split(".") == "mp3":
+    elif file_name[-3:] == "mp3":
         logging.info("Now playing: {}".format(file_name))
         duration = get_mp3_duration(file_name)
-        os.system("gnome-open {}".format(file_name))
+        os.system("gnome-open \"{}\"".format(file_name))
         time.sleep(duration + 1)
         
